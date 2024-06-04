1f6b3a2954f0e8c4270bc43ba2ca75b1464e794e
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2024-06-04 15:31:10 +0100

    chore: remove unused themes

--- nvim/lua/user/plugins/init.lua
+++ nvim/lua/user/plugins/init.lua
@@ -1,27 +1,5 @@
 return {
   "AstroNvim/astrocommunity",
   "folke/tokyonight.nvim",
-  "joshdick/onedark.vim",
-  { import = "astrocommunity.colorscheme.catppuccin", enabled = false },
-  { import = "astrocommunity.colorscheme.rose-pine", enabled = false },
-  {
-    "rose-pine",
-    opts = {
-      variant = "main",
-      dark_variant = "main",
-      styles = {
-        bold = false,
-        italic = true,
-        transparency = true,
-      },
-    },
-  },
-  {
-    "catppuccin",
-    opts = {
-      flavour = "mocchiato",
-  --    transparent_background = false,
---      term_colors = false,
-    },
-  },
+  "joshdick/onedark.vim"
 }
