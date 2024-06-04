537e122fa7fc50b194b5fbb7b1fade6974703722
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2024-06-04 15:12:13 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+curl -fsSL https://fnm.vercel.app/install | bash
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+ln -s ~/.config/.tmux.conf ~/tmux.conf
+
+ln -s ~/.config/.zshrc ~/.zshrc
+
+ln -s ~/.config/antigen/ ~/.antigen
+
