4a76be13e86884a70fc2846a2912b0fd7742fd55
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2024-06-27 10:38:34 +0100

    chore: update wezterm to no borders

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,12 +8,13 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
 	left = 0,
 	right = 0,
-	top = 0,
+	-- top = 0,
 	bottom = 0,
 }
 
 config.term = "xterm-256color"
 config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
 
 config.default_prog = { "/usr/local/bin/tmux", "-l" }
 
