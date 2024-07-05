7ce174777f106bb6131d0036a0eae5ff3980e53e
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2024-07-05 11:10:32 +0100

    chore: change wezterm to catppuccin

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Tokyo Night"
--- config.color_scheme = "Tokyo Night Day"
+-- config.color_scheme = "Tokyo Night"
+config.color_scheme = "Catppuccin Mocha"
 
 config.hide_tab_bar_if_only_one_tab = true
 
